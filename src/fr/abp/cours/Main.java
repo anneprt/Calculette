9c8789ed473861a24e88a46d72bc750eaceb113c
Anne <patesdegeek@gmail.com>
2020-04-14 21:20:03 +0200

    declaration des variables

--- src/fr/abp/cours/Main.java
+++ src/fr/abp/cours/Main.java
@@ -1,8 +1,16 @@
 package fr.abp.cours;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int a;
+        int b;
+        char operateur;
+        double calcul = 0;
+
+        Scanner lectureClavier = new Scanner(System.in);
+        
     }
 }
